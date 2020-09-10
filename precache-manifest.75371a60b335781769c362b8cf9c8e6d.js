e22610652c126461f22953f98ecf4ab7be3f0fc1
Kallox <d34d.mc@gmail.com>
2020-09-10 03:32:34 -0300

    Updates

---
+++ precache-manifest.75371a60b335781769c362b8cf9c8e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7ea7fda0e94c389dae83a7d9a2135b",
+    "url": "/learning-reactjs-basic/index.html"
+  },
+  {
+    "revision": "7f127bfcb2800eea2613",
+    "url": "/learning-reactjs-basic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7c76cc6242b3c9a8dc6b",
+    "url": "/learning-reactjs-basic/static/js/2.65c4520e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learning-reactjs-basic/static/js/2.65c4520e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f127bfcb2800eea2613",
+    "url": "/learning-reactjs-basic/static/js/main.05818b57.chunk.js"
+  },
+  {
+    "revision": "0bdd39cae39d66e4b3e5",
+    "url": "/learning-reactjs-basic/static/js/runtime-main.2f1e7c05.js"
+  }
+]);
